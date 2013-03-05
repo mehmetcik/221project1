dccd8118a8ad47c3edc42dfadde057f75947498b
saocode <saospam@gmail.com>
2013-03-04 21:32:42 -0800

    Plain C Project (Eclipse) now...  not Hello World.

---
+++ project1/test.C
@@ -0,0 +1,15 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//  cout << "Enter your name\n";
+//  string name;
+//  string last;
+//  getline(cin, name);
+//  cout << "Last?\n";
+//  getline (cin, last);
+//  cout << "Hello " << name << " " << last
+//       << " - welcome to C++\n";
+//  return 0;
+//}
